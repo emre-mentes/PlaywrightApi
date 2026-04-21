dd904b467b011ebfafde834601b1ed48796793ef
Emre MENTES <emreowen57@gmail.com>
2026-04-21 08:42:04 +0200

    changes

---
+++ tests/day01/01-request-response-explore.spec.ts
@@ -0,0 +1,15 @@
+/*
+    1) Manual API testleri icin Postman kullanmaya devam edeceğiz
+    2) Otomasyon API testleri icin playwright kullanacağız
+    3) API testleri yaparken yapacağımz islem sundan ibarettir
+        a) Send request and get response
+        b) Do assertion
+*/
+
+import { test } from '@playwright/test';
+
+test('request - response - explore', async ({ page }) => {
+    
+    
+    
+});
